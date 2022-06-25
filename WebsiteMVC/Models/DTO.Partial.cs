21674473ba64f028f434163b7e898edcea2e921a
học phùng <phungdai.hoc@hotmail.com>
2022-06-26 00:35:26 +0700

    Add project files.

---
+++ WebsiteMVC/Models/DTO.Partial.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebsiteMVC.Models
+{
+    public class BaseInfo
+    {
+        public Address xAddress1 { get; set; }
+        public Address xAddress2 { get; set; }
+        public Address xAddress3 { get; set; }
+    }
+
+    public partial class DuAn : BaseInfo
+    {
+        public ImageDuAn xImage { get; set; }
+    }
+
+    public partial class House : BaseInfo
+    {
+        public ImageHouse xImage { get; set; }
+        public DateTime CreateTime { get; internal set; }
+    }
+}
